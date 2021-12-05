960f5a06cb7b191638347362e48c95b1ee8f4175
NicholasYe <2216393769@qq.com>
2021-12-05 12:18:45 +0800

    12.5

--- Math/linear_regression.m
+++ Math/linear_regression.m
@@ -8,11 +8,20 @@ xlabel('x(职工工资总额)', 'FontSize', 12)
 ylabel('y(商品零售总额)', 'FontSize', 12)
 set(gca, 'linewidth', 2);
 
-% 采用最小二乘拟合
-Lxx = sum((x-mean(x)).^2);
-Lxy = sum((x-mean(x)).*(y-mean(y)));
-b1 = Lxy/Lxx;
-b0 = mean(y)- b1*mean(x);
-y1 = b1*x + b0;
-hold on
-plot(x, y1, 'LineWidth',2)
+% % 采用最小二乘拟合
+% Lxx = sum((x-mean(x)).^2);
+% Lxy = sum((x-mean(x)).*(y-mean(y)));
+% b1 = Lxy/Lxx;
+% b0 = mean(y)- b1*mean(x);
+% y1 = b1*x + b0;
+% hold on
+% plot(x, y1, 'LineWidth',2)
+% 
+% % 采用LinearModel.fit函数进行线性回归
+% m2 = LinearModel.fit(x,y)
+
+% 采用regress函数进行回归
+Y = y';
+X = [ones(size(x,2),1),x'];
+[b, bint, r, rint, s] = regress(Y, X)
+
